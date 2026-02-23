b3b678bb06bcdf379cee2feb9d54e2f9de832a7d
harishammache <hammache_haris@hotmail.fr>
2026-02-23 14:14:39 +0100

    Implement user authentication logic in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  <div className='App'></div>
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [UserUsername, setUserUsername] = useState("")
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken")
+
+    if (token){
+      fetch("api/auth/", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json"
+        },
+      })
+      .then(res => res.json())
+      .then(data => {
+        setIsLoggedIn(true);
+        setUsername(data.username);
+      })
+      .catch(() => {
+        setIsLoggedIn(false);
+        setUserUsername("");
+      })
+    }
+  },)
+  return (
+    <div>
+      {isLoggedIn ? (
+        <Dashboard />
+      ) : (
+        <Authentication />
+      )}
+    </div>
+  );
 }
 export default App
